3b0bda49dab193289b96a150bd3fd7dfd846240c
paulohenrique085 <paulohnq85@outlook.com>
2021-02-24 21:18:13 -0300

    atualizacao da tela de login

--- lib/tela_login.dart
+++ lib/tela_login.dart
@@ -23,43 +23,68 @@ class _tela_loginState extends State<tela_login> {
                 alignment: Alignment.topCenter,
                 child: Image.asset(
                   "assets/log.png",
-                  width: 230,
-                  height: 230,
+                  width: 250,
+                  height: 250,
                 ),
               ),
               /////////////////////////////////////////////////////////////
-
+              //container que recebe a coluna com os textfild
               Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(10))),
-                child: TextField(
-                  decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderRadius: const BorderRadius.all(
-                          const Radius.circular(10),
+                width: 300,
+                height: 200,
+                color: Colors.black87,
+                padding: EdgeInsets.only(top: 20, right: 30, left: 25),
+                child: Center(
+                  //coluna com os textsfilds
+                  child: Column(
+                    //email
+                    children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                borderRadius: const BorderRadius.all(
+                                  const Radius.circular(10),
+                                ),
+                              ),
+                              hintText: 'E-mail'),
                         ),
                       ),
-                      hintText: 'E-mail'),
-                ),
-              ),
 
-              Container(
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(10))),
-                child: TextField(
-                  decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      border: OutlineInputBorder(
-                        borderRadius: const BorderRadius.all(
-                          const Radius.circular(10),
+                      //senha
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                borderRadius: const BorderRadius.all(
+                                  const Radius.circular(10),
+                                ),
+                              ),
+                              hintText: 'Senha'),
                         ),
                       ),
-                      hintText: 'Senha'),
+                      RaisedButton(
+                        child: Text(
+                          "Login",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        onPressed: () {},
+                        color: Colors.amber,
+                      )
+                    ],
+                  ),
                 ),
-              ),
+              )
             ],
           ),
         ),
