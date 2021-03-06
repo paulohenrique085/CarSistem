e5edc310a08e6244ed1c1049f74bb41b4e38fb68
paulohenrique085 <paulohnq85@outlook.com>
2021-03-06 12:54:47 -0300

    atualizacaotela

--- lib/tela_login.dart
+++ lib/tela_login.dart
@@ -12,83 +12,111 @@ class _tela_loginState extends State<tela_login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 30),
-        child: SingleChildScrollView(
+        backgroundColor: Colors.amber,
+        body: SingleChildScrollView(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              //alinhamento da logo acima
-              Align(
-                alignment: Alignment.topCenter,
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 30, bottom: 30, left: 110, right: 110),
                 child: Image.asset(
-                  "assets/log.png",
-                  width: 250,
-                  height: 250,
+                  "assets/Yan.png",
+                  width: 90,
+                  height: 90,
                 ),
               ),
-              /////////////////////////////////////////////////////////////
-              //container que recebe a coluna com os textfild
-              Container(
-                width: 300,
-                height: 200,
-                color: Colors.black87,
-                padding: EdgeInsets.only(top: 20, right: 30, left: 25),
-                child: Center(
-                  //coluna com os textsfilds
+              Padding(
+                padding: const EdgeInsets.only(
+                    top: 10, bottom: 10, right: 30, left: 30),
+                child: Container(
+                  height: 350,
+                  width: 400,
+                  decoration: BoxDecoration(
+                      color: Colors.black87,
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
                   child: Column(
-                    //email
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: <Widget>[
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              fillColor: Colors.white,
-                              border: OutlineInputBorder(
-                                borderRadius: const BorderRadius.all(
-                                  const Radius.circular(10),
-                                ),
-                              ),
-                              hintText: 'E-mail'),
+                      //container alinhado estilizado para campo:EMAIl
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(top: 30, left: 20, right: 20),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(12))),
+                          child: TextField(
+                            decoration: InputDecoration(hintText: "E-mail"),
+                          ),
                         ),
                       ),
-
-                      //senha
-                      Container(
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              fillColor: Colors.white,
-                              border: OutlineInputBorder(
-                                borderRadius: const BorderRadius.all(
-                                  const Radius.circular(10),
-                                ),
-                              ),
-                              hintText: 'Senha'),
+                      //container alinhado estilizado para campo:SENHA
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(top: 20, left: 20, right: 20),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(12))),
+                          child: TextField(
+                            decoration: InputDecoration(hintText: "Senha"),
+                          ),
                         ),
                       ),
-                      RaisedButton(
-                        child: Text(
-                          "Login",
-                          style: TextStyle(color: Colors.black),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: GestureDetector(
+                          onTap: () {},
+                          child: Text(
+                            "Esqueci a senha",
+                            style: TextStyle(color: Color(0xFF16A085)),
+                          ),
                         ),
-                        onPressed: () {},
-                        color: Colors.amber,
-                      )
+                      ),
+                      //botão entrar
+                      Padding(
+                          padding: const EdgeInsets.only(
+                              top: 25, left: 20, right: 20),
+                          child: Container(
+                            width: 400,
+                            child: RaisedButton(
+                              //arredondando bordas do botão
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12)),
+                              color: Color(0xFF16A085),
+                              onPressed: () {},
+                              child: Text(
+                                "Entrar",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          )),
+                      //botão cadastrar
+                      Padding(
+                          padding: const EdgeInsets.only(left: 20, right: 20),
+                          child: Container(
+                            width: 400,
+                            child: RaisedButton(
+                              //arredondando bordas do botão
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12)),
+                              color: Color(0xFF16A085),
+                              onPressed: () {},
+                              child: Text(
+                                "Cadastrar",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ))
                     ],
                   ),
                 ),
-              )
+              ),
             ],
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
